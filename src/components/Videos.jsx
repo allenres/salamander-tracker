6ab7dfced7c8255a2b702d0045831cb0a85f181b
Akaioka <brendanvillaraza@yahoo.com>
2026-06-02 10:39:54 -0700

    Change file imports

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getVideos } from '../mockApi.js';
+import { getVideos } from '../api.js';
 import { useNavigate } from "react-router-dom";
 
 function Videos() {
